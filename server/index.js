55ce84b290a2c7d3c70c2dc76689adb2a2960b42
MalwandlaShaun <hlungwanimalwandlashaun@gmail.com>
2023-09-14 12:28:25 +0200

    I was editing the index no big deal

--- server/index.js
+++ server/index.js
@@ -26,6 +26,7 @@ app.use((req, res, next) => {
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
   );
+  res.header("Access-Control-Allow-Credentials", "true"); // Allow credentials
   next();
 });
 app.use(express.json());
