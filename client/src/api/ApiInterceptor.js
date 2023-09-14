c5b502066f7a57a1cff1c030f91b90b7a36da12e
MalwandlaShaun <hlungwanimalwandlashaun@gmail.com>
2023-09-14 12:19:44 +0200

    deploy hosting

--- client/src/api/ApiInterceptor.js
+++ client/src/api/ApiInterceptor.js
@@ -1,7 +1,10 @@
 import axios from "axios";
 //import { API_BASE_URL } from './ApiConstant';
 
-export const API = axios.create({ baseURL: "https://hotet-app-api-2-0.onrender.com" });
+export const API = axios.create({
+  baseURL: "https://hotet-app-api-2-0.onrender.com",
+  withCredentials: true,
+});
 API.interceptors.request.use((req) => {
   if (localStorage.getItem("user")) {
     req.headers.Authorization = `Bearer ${
