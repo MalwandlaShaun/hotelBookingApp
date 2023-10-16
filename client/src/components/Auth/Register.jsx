cc6259460d4f9121d3c6adc48f139a63711d5b6d
MalwandlaShaun <hlungwanimalwandlashaun@gmail.com>
2023-10-16 13:26:26 +0200

    navigation 1

--- client/src/components/Auth/Register.jsx
+++ client/src/components/Auth/Register.jsx
@@ -43,6 +43,7 @@ const Register = () => {
         "https://banner2.cleanpng.com/20180329/zue/kisspng-computer-icons-user-profile-person-5abd85306ff7f7.0592226715223698404586.jpg",
     };
     signUpUser(newUserData);
+    navigate(-1);
   };
 
   // Upload Cover Room Image
